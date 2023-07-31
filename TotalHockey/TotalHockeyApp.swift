3b675e233a5b870228a8d911a23e549d30954855
Arthur Ford <adf.ford@Gmail.com>
2023-07-31 11:11:36 -0400

    Initial Commit

---
+++ TotalHockey/TotalHockeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalHockeyApp.swift
+//  TotalHockey
+//
+//  Created by Artie Ford on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct TotalHockeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
